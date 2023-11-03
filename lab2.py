e610db62b4c8d3d7417af35620998e44c366a797
Bartosz Hanc <bartosz.hanc02@gmail.com>
2023-11-03 21:36:26 +0100

    update

--- lab2.py
+++ lab2.py
@@ -13,13 +13,6 @@ if __name__ == "__main__":
     parser = MyParser()
     lexer = MyLexer()
     text = file.read()
-    text = """
-A = zeros(5); # create 5x5 matrix filled with zeros
-D = A.+B' ;   # add element-wise A with transpose of B
-
-for j = 1:10 
-    print j;
-"""
     tokens = lexer.tokenize(text=text)
     result = parser.parse(tokens)
     print(result)
